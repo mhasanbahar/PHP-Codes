a495ffd9a5de0bbf2e23c92ac98658a0ba38e78f
Muhammet Hasan Bahar <121157931+mhasanbahar@users.noreply.github.com>
2024-01-10 18:30:30 +0300

    Understanding index logic

---
+++ indexLoop.php
@@ -0,0 +1,10 @@
+<?php
+    $cars = array("Volvo", "BMW", "Toyota");
+    $length = count($cars);
+
+    for ($x = 0; $x < $length; $x++)
+    {
+        echo $cars[$x];
+        echo "<br>";
+    }
+?>
